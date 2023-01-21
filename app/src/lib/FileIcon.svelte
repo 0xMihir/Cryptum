9af3c04b1b81d05427699c5b6714346e8363f39b
jack <jack.x.roscoe@gmail.com>
2023-01-21 13:51:48 -0500

    changed file icon

--- app/src/lib/FileIcon.svelte
+++ app/src/lib/FileIcon.svelte
@@ -3,6 +3,15 @@
     export let selected: boolean;
 </script>
 
-<div on:click class:text-bg-primary={selected}>
+<div on:click class:text-bg-primary={selected} class="rounded file-icon">
     <p>{name}</p>
 </div>
+
+<style>
+    .file-icon {
+        width: 7rem;
+        height: 9rem;
+        padding: 1rem;
+        margin: 1rem;
+    }
+</style>
