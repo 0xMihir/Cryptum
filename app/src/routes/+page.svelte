d0e561c314e36d9fcf9c7ffed32386542f67e2c3
Nick Andry <nicholas.andry33@gmail.com>
2023-01-21 02:33:00 -0500

    Finished barebones upload button and post upload feature. Added user_files to the .gitignore

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import {onMount} from 'svelte';
+	import Upload from '../lib/Upload.svelte';
 
     onMount(() => {
         console.log('Hello from SvelteKit');
@@ -9,5 +10,6 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Upload/>
 
 
--- app/src/routes/files/+server.ts
+++ app/src/routes/files/+server.ts
@@ -1,27 +1,28 @@
-import type { RequestEvent } from "@sveltejs/kit";
-import { error } from "@sveltejs/kit";
+import type { RequestEvent } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import { randomUUID } from 'crypto';
 import { join } from 'path/posix';
-import { writeFile } from "fs/promises";
+import { writeFile } from 'fs/promises';
 
-import { STORAGE_DIR } from "../../lib/server/config";
+import { STORAGE_DIR } from '../../lib/server/config';
 
 // creates a new file and returns the uuid
 // file data will be request body
 export async function POST(request: RequestEvent): Promise<Response> {
-    const uuid = randomUUID();
-    const filePath = join(STORAGE_DIR, uuid);
+	const uuid = randomUUID();
+	const filePath = join(STORAGE_DIR, uuid);
 
-    const data = (await request?.request?.body?.getReader().read())?.value;
-    if (data == null) {
-        throw error(400, "body required");
-    }
+	const data = (await request?.request?.body?.getReader().read())?.value;
 
-    try {
-        await writeFile(filePath, data);
-    } catch (e) {
-        throw error(500, "could not write data to file");
-    }
+	if (data == null) {
+		throw error(400, 'body required');
+	}
 
-    return new Response(uuid);
+	try {
+		await writeFile(filePath, data);
+	} catch (e) {
+		throw error(500, 'could not write data to file');
+	}
+
+	return new Response(uuid);
 }
