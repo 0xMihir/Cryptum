b1984b8abed4cfe86bf82207ac40d0662c8b2d82
jack <jack.x.roscoe@gmail.com>
2023-01-21 01:38:38 -0500

    added some new routes

---
+++ app/src/routes/files/root/+server.ts
@@ -0,0 +1,13 @@
+import type { RequestEvent } from "@sveltejs/kit";
+
+// returns the root folder for the current user
+export async function GET(RequestEvent: RequestEvent): Promise<Response> {
+    // TODO
+    return new Response();
+}
+
+// set the root folder for the current user
+export async function POST(RequestEvent: RequestEvent): Promise<Response> {
+    // TODO
+    return new Response();
+}
