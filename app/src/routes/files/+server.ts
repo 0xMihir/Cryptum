a3e4cca113928a408c69fac77a9e22bd89147fa3
jack <jack.x.roscoe@gmail.com>
2023-01-21 20:29:50 -0500

    refactored code with uploading and getting files

--- app/src/routes/files/+server.ts
+++ app/src/routes/files/+server.ts
@@ -1,37 +1,19 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import { error } from '@sveltejs/kit';
 import { randomUUID } from 'crypto';
-import { join } from 'path/posix';
-import { writeFile } from 'fs/promises';
-
-import { STORAGE_DIR } from '../../lib/server/config';
-import { Storage } from '@google-cloud/storage';
+import { createObject } from '$lib/server/googleCloud';
 
 // creates a new file and returns the uuid
 // file data will be request body
 export async function POST(request: RequestEvent): Promise<Response> {
 	const uuid = randomUUID();
 
-	const storage = new Storage({
-		projectId: 'cryptum',
-		keyFilename: 'gc_keyfile/cryptum-cc63e7b7ac73.json'
-	});
-
-	const bucket = storage.bucket('cryptum-storage');
-
 	const data = (await request?.request?.body?.getReader().read())?.value;
+	if (data == null) {
+		throw error(400, "no data sent for file");
+	}
 
-	let contents = data as Buffer;
-	const blob = bucket.file(uuid);
-	const blobStream = blob.createWriteStream({
-		resumable: false
-	});
-
-	await blobStream
-		.on('error', () => {
-			throw error(500, 'unable to upload file to cloud');
-		})
-		.end(contents);
+	await createObject(uuid, data);
 
 	return new Response(uuid);
 }
--- app/src/routes/files/root/+server.ts
+++ app/src/routes/files/root/+server.ts
@@ -1,13 +1,25 @@
 import type { RequestEvent } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
+import { getUserFileUuid } from '$lib/server/db';
+
+async function getUserRootUuid(requestEvent: RequestEvent): Promise<string> {
+	const pubKey = (await requestEvent.request.json())['uuid'];
+	const uuid = await getUserFileUuid(pubKey);
+	if (uuid != null) {
+		return uuid;
+	} else {
+		throw error(400, "not logged in");
+	}
+}
 
 // returns the root folder for the current user
-export async function GET(RequestEvent: RequestEvent): Promise<Response> {
-	// TODO
+export async function GET(requestEvent: RequestEvent): Promise<Response> {
+	const uuid = await getUserRootUuid(requestEvent);
 	return new Response();
 }
 
 // set the root folder for the current user
-export async function POST(RequestEvent: RequestEvent): Promise<Response> {
-	// TODO
+export async function POST(requestEvent: RequestEvent): Promise<Response> {
+	const uuid = await getUserRootUuid(requestEvent);
 	return new Response();
 }
