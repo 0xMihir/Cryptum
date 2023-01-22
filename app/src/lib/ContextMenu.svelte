0d4866ac55cff706be6c7c38acc93f17841efec8
jack <jack.x.roscoe@gmail.com>
2023-01-22 03:33:50 -0500

    added file moving

--- app/src/lib/ContextMenu.svelte
+++ app/src/lib/ContextMenu.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { FormGroup, Label } from 'sveltestrap';
 	import { createEventDispatcher } from 'svelte';
-	import type { Directory, INode } from './directoryTree';
+	import { ConfirmError, ConfirmOk, newConfirm } from './confirm/confirm';
+	import Confirmation from './confirm/Confirmation.svelte';
+	import { Directory, type INode } from './directoryTree';
 	import { File } from './directoryTree';
+	import SearchSelect from './SearchSelect.svelte';
 	const dispatch = createEventDispatcher();
 
 	export let currentDirectory: Directory;
@@ -10,8 +14,63 @@
 	export let y: Number;
 	$: menuStyle = "position: absolute; top: " + y.toString() + "px; left: " + x.toString() + "px;";
 
+	let delayCloseMenu = false;
+
+	interface Option {
+        name: string,
+        value: any,
+    }
+	const moveConfirm = newConfirm();
+	let moveToOptions: Option[] = [];
+	let selectedLocation: Directory | null = null;
 	function moveNode() {
-		console.log("file move");
+		delayCloseMenu = true;
+
+		moveToOptions = [];
+		for (const key in currentDirectory.children) {
+			const child = currentDirectory.children[key];
+			if (child instanceof Directory) {
+				moveToOptions.push({
+					name: key,
+					value: child,
+				});
+			}
+		}
+		
+		if (currentDirectory.parent != null) {
+			moveToOptions.push({
+				name: currentDirectory.parent.name + " (Parent)",
+				value: currentDirectory.parent,
+			});
+		}
+
+		moveConfirm.confirm(async confirmed => {
+			if (confirmed) {
+				if (selectedLocation == null) {
+					return ConfirmError("please select a directory to move to");
+				}
+
+				if (selectedLocation.hasFile(node.name)) {
+					return ConfirmError("this directory already contains a file with the same name");
+				}
+
+				currentDirectory.removeChildInode(node);
+				selectedLocation.addChild(node);
+
+				// without this timeout, we have graphical glitches
+				setTimeout(() => {
+					dispatch('closemenu');
+					delayCloseMenu = false;
+				}, 500);
+				
+				dispatch("foldersUpdated");
+				return ConfirmOk;
+			} else {
+				dispatch('closemenu');
+				delayCloseMenu = false;
+				return ConfirmOk;
+			}
+		});
 	}
 
 	async function removeNode() {
@@ -31,11 +90,24 @@
 	</div>
 </div>
 
-<svelte:window on:click={() => dispatch('closemenu')}/>
+<Confirmation header="Move File" confirmationStore={moveConfirm}>
+	<FormGroup>
+		<Label for="destination">Select Destination</Label>
+		<SearchSelect id="destination" bind:value={selectedLocation} options={moveToOptions}/>
+	</FormGroup>
+</Confirmation>
+
+<svelte:window on:click={() => {
+	if (!delayCloseMenu) {
+		dispatch('closemenu');
+	}
+}}/>
 
 <style>
 	.option-container {
-		border: 2px black solid
+		border-radius: 5px;
+		border: 2px lightgrey solid;
+		overflow: hidden;
 	}
 
 	.option {
@@ -46,7 +118,7 @@
 	}
 
 	.option:hover {
-		background-color: grey;
+		background-color: lightgrey;
 	}
 	
 </style>
